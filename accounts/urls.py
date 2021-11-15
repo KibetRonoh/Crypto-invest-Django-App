89a93c14b5c2acd3618f45205c77910f7b950dd3
‘Kibet <kibethillaire@gmail.com>
2021-11-15 20:22:53 +0300

    On branch master

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,3 +8,14 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard')
 ]
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard')
+]

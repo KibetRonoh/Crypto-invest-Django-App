2a80a8945a802b994f60edd173c12982e574eb7e
‘Kibet <kibethillaire@gmail.com>
2021-11-21 19:57:08 +0300

    slight update on urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,14 +8,3 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard')
 ]
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('dashboard', views.dashboard, name='dashboard')
-]
